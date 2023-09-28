d7ada0fcf1eaaa268a4b1cb42d5a4c406d7e3b96
Replit user <>
2023-09-28 13:37:37 +0000

    new try

--- heap.c
+++ heap.c
@@ -34,7 +34,7 @@ void heap_push(Heap* pq, void* data, int priority){
   }
   heapElem Nuevo;
   Nuevo.data = data;
-  Nuevo.priority = pq;
+  Nuevo.priority = p;
 
   int indice = pq->size;
   pq->heapArray[indice] = Nuevo;
